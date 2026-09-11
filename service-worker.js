045306f5c9ed260068abbaedb97759cb83bf066a
Merrdo <mertkutlu26@gmail.com>
2026-09-11 22:32:58 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -5,7 +5,7 @@
 // yapılan güncellemeler CACHE_VERSION hiç değişmese bile bir sonraki
 // açılışta görünür.
 
-const CACHE_VERSION = 'takvimim-v1';
+const CACHE_VERSION = 'takvimim-v2';
 const CORE_ASSETS = [
   './manifest.json',
   './icons/favicon.png',
